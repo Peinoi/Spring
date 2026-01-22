a2f47f15cc7eb09a8ad0b05d960397f3eca6f50c
master <mymail@google.com>
2026-01-22 17:51:59 +0900

    docker

---
+++ sp05-1/src/main/java/com/yedam/app/CustomerController.java
@@ -0,0 +1,24 @@
+package com.yedam.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+	@Autowired
+	CustomerRepository repository;
+
+	@GetMapping("/")
+	public Iterable<Customer> list() {
+		return repository.findAll();
+
+	}
+
+	@GetMapping("/find")
+	public Iterable<Customer> listname(String name) {
+		return repository.findByFirstNameLike(name+"%");
+
+	}
+}
